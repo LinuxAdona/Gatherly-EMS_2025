75064679ddaf7853ec17e9e8eff402043fa31fc4
Linux Adona <linuxadona17@gmail.com>
2025-10-27 11:10:00 +0800

    Segregated folders for organization

---
+++ src/pages/signin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in to Gatherly | Gatherly</title>
+    <link rel="icon" type="image/x-icon" href="../../public/assets/images/logo.png">
+    <link rel="stylesheet" href="../output.css">
+    <script src="https://kit.fontawesome.com/2a99de0fa5.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-['Montserrat']">
+        <div class="flex flex-col items-center font-bold">
+            <img class="w-16 mb-8" src="../../public/assets/images/logo.png" alt="Logo">
+            Sign in to your account
+        </div>
+
+    </div>
+</body>
+
+</html>
